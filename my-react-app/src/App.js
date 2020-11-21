5e43952690cf6c6f8c28758d1ffd87d58238e12d
ryanbrowne360 <ryanbrowne360@gmail.com>
2020-11-21 13:56:41 -0500

    removed extra folder and imported old html

--- my-react-app/src/App.js
+++ my-react-app/src/App.js
@@ -1,24 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import Navbar from "./components/Navbar";
+import Portfolio from "./views/Portfolio";
+import Contact from "./views/Contact";
+import Home from "./views/Home";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+//import {Route, Switch} from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+    
+      <div>
+        <Switch>
+          <Route exact path="/">
+            <Home />
+          </Route>
+          <Route path="/contact">
+            <Contact/>
+          
+          </Route>
+        </Switch>
+      </div>
+    </BrowserRouter>
   );
 }
 
---
+++ my-react-app/src/views/Home.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Home() {
+  return (
+    <div>
+      <h1>Ryan's Developer Portfolio</h1>
+    </div>
+  );
+}
+
+export default Home;
