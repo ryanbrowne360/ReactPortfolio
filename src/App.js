9e24e5b3b12a5cda0df6e3bce8186c5962ae40e1
ryanbrowne360 <ryanbrowne360@gmail.com>
2020-11-23 21:34:03 -0500

    wrapped in navbar and changed browserrouter to hashrouter

--- src/App.js
+++ src/App.js
@@ -8,25 +8,22 @@ import { HashRouter, Link, BrowserRouter, Switch, Route } from "react-router-dom
 
 function App() {
     return (
-        <BrowserRouter basename='/'>
-
-            <div className="App">
-                <div>
-                    <Navbar />
-
-                    <Switch>
-
-                        <Route exact path="/" component={Home} />
-                        <Route exact path="/contact" component={Contact} />
-                        <Route exact path="/portfolio" component={Portfolio} />
-
-                    </Switch>
-                </div>
-                <div class="container"><Footer /></div>
-
-            </div>
-        </BrowserRouter>
-
+        <HashRouter basename={process.env.PUBLIC_URL}>
+        <div className="App">
+          <div>
+            <Navbar />
+  
+            <Switch>
+              <Route exact path="/" component={Home} />
+              <Route exact path="/contact" component={Contact} />
+              <Route exact path="/portfolio" component={Portfolio} />
+            </Switch>
+          </div>
+          <div class="container">
+            <Footer />
+          </div>
+        </div>
+      </HashRouter>
     );
-}
+  }
 export default App;
